51968160633f3aa61072049da4d2710aea8ca43c
omarhuanca <oma378501@yahoo.com>
2020-08-20 12:06:00 -0400

    setup environment project.

---
+++ functions/src/index.ts
@@ -0,0 +1,44 @@
+import { Request , Response } from 'express';
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+const cors = require('cors')({origin: true});
+admin.initializeApp();
+
+/**
+* Here we're using Gmail to send 
+*/
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'websolutionssrl@gmail.com',
+        pass: 'roder12345tis'
+    }
+});
+
+exports.sendMail = functions.https.onRequest((req: Request, res: Response) => {
+    cors(req, res, () => {
+      
+        // getting dest email by query string
+        const dest = req.query.dest;
+
+        const mailOptions = {
+            from: 'Your Account Name <websolutionssrl@gmail.com>', // Something like: Jane Doe <janedoe@gmail.com>
+            to: dest,
+            subject: 'I\'M A PICKLE!!!', // email subject
+            html: `<p style="font-size: 16px;">Pickle Riiiiiiiiiiiiiiiick!!</p>
+                <br />
+                <img src="https://images.prod.meredith.com/product/fc8754735c8a9b4aebb786278e7265a5/1538025388228/l/rick-and-morty-pickle-rick-sticker" />
+            ` // email content in HTML
+        };
+  
+        // returning result
+        return transporter.sendMail(mailOptions, (erro: any, info: any) => {
+            if(erro){
+                return res.send(erro.toString());
+            }
+            return res.send('Sended');
+        });
+    });    
+});
